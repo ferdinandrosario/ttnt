3c2cca9679d5b2d8758ac44e5fc1efa9c75d0798
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-17 11:36:26 +0900

    Remove unneeded class var and make some methods private

--- lib/ttnt/testtask.rb
+++ lib/ttnt/testtask.rb
@@ -4,19 +4,6 @@ require 'ttnt/test_selector'
 
 module TTNT
   class TestTask < Rake::TestTask
-    @@instances = []
-
-    def copy_instance_variables(src, dest)
-      ivars = src.instance_variables
-      ivars.map! { |ivar| ivar[1..-1] }
-
-      ivars.each do |ivar|
-        if dest.respond_to?(:"#{ivar}=") && src.respond_to?(:"#{ivar}")
-          dest.send(:"#{ivar}=", src.send(:"#{ivar}"))
-        end
-      end
-    end
-
     def initialize(rake_test_task)
       copy_instance_variables(rake_test_task, self)
       # Since test_files is not exposed in Rake::TestTask
@@ -27,6 +14,8 @@ module TTNT
       @run_description = 'Run selected tests' + (@name == :test ? '' : "for #{@name}")
       define_tasks
     end
+
+    private
 
     # Task definitions are taken from Rake::TestTask
     # https://github.com/ruby/rake/blob/e644af3/lib/rake/testtask.rb#L98-L112
@@ -85,6 +74,17 @@ module TTNT
               end
             end
           end
+        end
+      end
+    end
+
+    def copy_instance_variables(src, dest)
+      ivars = src.instance_variables
+      ivars.map! { |ivar| ivar[1..-1] }
+
+      ivars.each do |ivar|
+        if dest.respond_to?(:"#{ivar}=") && src.respond_to?(:"#{ivar}")
+          dest.send(:"#{ivar}=", src.send(:"#{ivar}"))
         end
       end
     end
